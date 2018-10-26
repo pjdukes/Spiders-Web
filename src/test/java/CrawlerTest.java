0c992041d37da1521d8bb8e6680057598bb75325
Jack Bergman <jrbergma@mtu.edu>
2018-10-25 22:54:33 -0400

    small changes to the tests

--- src/test/java/CrawlerTest.java
+++ src/test/java/CrawlerTest.java
@@ -5,10 +5,10 @@ import Spiders-Web.*;
 
 
 public class CrawlerTest {
-	Crawler test = new Crawler();
 	
 	@Test
 	public void testLinkedListSize() {
+		Crawler test = new Crawler();
 		ArrayList<String> linkList = new ArrayList<>();
 		test.crawl("http://www.google.com", linkList, 1);
 		assertTrue("The size should be greater than 0", linkList.size() >= 1);
