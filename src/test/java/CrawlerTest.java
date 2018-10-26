84470a0da4810e5ad92269f6aa777d685b9a466d
Jack Bergman <jrbergma@mtu.edu>
2018-10-25 23:08:33 -0400

    Fixed some of the test cases to test the proper thing

--- src/test/java/CrawlerTest.java
+++ src/test/java/CrawlerTest.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import static org.junit.Assert.*;
 import org.junit.Test;
-import Spiders-Web.*; 
+
 
 
 public class CrawlerTest {
@@ -12,7 +12,12 @@ public class CrawlerTest {
 		ArrayList<String> linkList = new ArrayList<>();
 		test.crawl("http://www.google.com", linkList, 1);
 		assertTrue("The size should be greater than 0", linkList.size() >= 1);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void testError() {
+		Crawler test = new Crawler();
+		ArrayList<String> linkList = new ArrayList<>();
 		test.crawl("bad.link", linkList, 1);
-		assertTrue("The size should be 0", linkList.size() == 0);
 	}
 }
