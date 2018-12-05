f1f7747d07fb491aca6c5ad7268174bffcd762c8
Trevor Good <tdgood98@gmail.com>
2018-12-05 14:54:34 -0500

    Changes to make Index Tests work

--- src/test/java/IndexTest.java
+++ src/test/java/IndexTest.java
@@ -9,14 +9,12 @@ public class IndexTest {
 
 	@Test
 	public void testIndexClosure() {
-		System.out.println("Test1");
-		assert(Index.indexMain("https://www.google.com/",1,1,false) == true);
+		assertTrue(Index.indexMain("https://www.google.com/",1,1,false));
 	}
 	
 	@Test
 	public void testIndexCharting() {
-		System.out.println("Test2");
-		assert(Index.indexRecord(true) == true);
+		assertTrue(Index.indexRecord(true));
 	}
 
 }
