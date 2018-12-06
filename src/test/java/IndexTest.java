f74d801ef0e5c44d3b15110276f723bfeeb00b33
Trevor Good <tdgood98@gmail.com>
2018-12-05 20:00:30 -0500

    More changes to make Index Tests work

--- src/test/java/IndexTest.java
+++ src/test/java/IndexTest.java
@@ -4,17 +4,21 @@ import static org.junit.Assert.*;
 import org.junit.Ignore;
 import org.junit.Test;
 import java.io.*;
+import java.sql.Connection;
 
 public class IndexTest {
 
 	@Test
 	public void testIndexClosure() {
-		assertTrue(Index.indexMain("https://www.google.com/",1,1,false));
+        System.setIn(new ByteArrayInputStream("3 excel.csv 4".getBytes())); //DON'T TOUCH THIS
+	    assertTrue(Index.indexMain("https://www.google.com/",1,1,false));
 	}
 
 	@Test
 	public void testIndexCharting() {
-		assertTrue(Index.indexRecord(true));
+        DBO db = new DBO();
+		Connection c = db.connectDB(true);
+		assertTrue(Index.indexRecord(true, db, c));
 	}
 
 }
